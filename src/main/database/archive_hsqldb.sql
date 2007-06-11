98e393877e4aa6031a1e85a33d0d30bb1a0e30e8
Stefan Reuter <stefan.reuter@reucon.com>
2007-06-11 10:25:13 +0000

    Added database script for hsqldb

---
+++ src/main/database/archive_hsqldb.sql
@@ -0,0 +1,35 @@
+CREATE TABLE archiveConversations (
+  conversationId        BIGINT          NOT NULL PRIMARY KEY,
+  startTime             BIGINT          NOT NULL,
+  endTime               BIGINT          NOT NULL,
+  roomJid               VARCHAR(255)
+);
+
+CREATE TABLE archiveParticipants (
+  participantId         BIGINT          NOT NULL PRIMARY KEY,
+  startTime             BIGINT          NOT NULL,
+  endTime               BIGINT,
+  jid                   VARCHAR(255)    NOT NULL,
+  nick                  VARCHAR(255),
+  conversationId        BIGINT          NOT NULL
+);
+CREATE INDEX idx_archiveParticipants_conversationId ON archiveParticipants (conversationId);
+
+CREATE TABLE archiveMessages (
+  messageId             BIGINT          NOT NULL PRIMARY KEY,
+  time                  BIGINT          NOT NULL,
+  originalId            VARCHAR(255),
+  fromJid               VARCHAR(255)    NOT NULL,
+  fromResource          VARCHAR(255),
+  toJid                 VARCHAR(255)    NOT NULL,
+  toResource            VARCHAR(255),
+  peerIpAddress         VARCHAR(255),
+  type                  CHAR(15)        NOT NULL,
+  thread                VARCHAR(255),
+  subject               VARCHAR(255),
+  body                  LONGVARCHAR,
+  conversationId        BIGINT NOT NULL
+);
+CREATE INDEX idx_archiveMessages_conversationId ON archiveMessages (conversationId);
+
+INSERT INTO jiveVersion (name, version) VALUES ('archive', 2);
